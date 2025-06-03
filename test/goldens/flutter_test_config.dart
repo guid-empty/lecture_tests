a1f98b7c5e71dc1c0714229feb80255433c4aa11
nt4f04uNd <nt4f04und@gmail.com>
2025-06-03 21:37:55 +0200

    +

--- test/goldens/flutter_test_config.dart
+++ test/goldens/flutter_test_config.dart
@@ -4,16 +4,12 @@ import 'package:alchemist/alchemist.dart';
 import 'package:flutter/material.dart';
 
 Future<void> testExecutable(FutureOr<void> Function() testMain) async {
-  const isRunningInCi = bool.fromEnvironment('CI', defaultValue: false);
-
   return AlchemistConfig.runWithConfig(
     config: AlchemistConfig.current().copyWith(
-      goldenTestTheme:
-          GoldenTestTheme.standard().copyWith(backgroundColor: Colors.white)
-              as GoldenTestTheme?,
-      platformGoldensConfig: const PlatformGoldensConfig(
-        enabled: !isRunningInCi,
-      ),
+      goldenTestTheme: GoldenTestTheme.standard()
+          .copyWith(backgroundColor: Colors.white) as GoldenTestTheme?,
+
+      /// platformGoldensConfig для CI...
     ),
     run: testMain,
   );
