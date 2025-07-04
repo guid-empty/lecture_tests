782a52d33fadab78bd5de02e08d2beeb22a523d3
dushesatwork <guid.empty@gmail.com>
2025-07-04 15:04:03 +0300

    mocktail in dev dependencies

--- test/data/todo_repository_test_7.dart
+++ test/data/todo_repository_test_7.dart
@@ -9,38 +9,49 @@ void main() {
 
   setUp(() {
     todoApiMock = TodoApiMock();
-    when(() => todoApiMock.saveTodo(any(), any()))
-        .thenAnswer((_) async => TodoModel.unknown);
+    when(
+      () => todoApiMock.saveTodo(any(), any()),
+    ).thenAnswer((_) async => TodoModel.unknown);
 
-    when(() => todoApiMock.saveTodo(
-            any(that: isA<String>()), any(that: isA<bool>())))
-        .thenAnswer((_) async => TodoModel.unknown);
+    when(() => todoApiMock.saveTodo('some title', false)).thenAnswer(
+      (_) async => TodoModel(id: -1, title: 'some title', isCompleted: false),
+    );
+
+    when(
+      () => todoApiMock.saveTodo(
+        any(that: isA<String>()),
+        any(that: isA<bool>()),
+      ),
+    ).thenAnswer((_) async => TodoModel.unknown);
 
     repository = TodoRepository(todoApi: todoApiMock);
   });
 
   group('TodoRepository', () {
     test(
-        'и его метод create должен выполнить обращение к TodoApi с правильными аргументами',
-        () async {
-      //  arrange
-      const expectedTitle = 'Новая задача';
-      const expectedIsCompleted = true;
+      'и его метод create должен выполнить обращение к TodoApi с правильными аргументами',
+      () async {
+        //  arrange
+        const expectedTitle = 'Новая задача';
+        const expectedIsCompleted = true;
 
-      //  act
-      await repository.create(
-        title: expectedTitle,
-        isCompleted: expectedIsCompleted,
-      );
+        //  act
+        await repository.create(
+          title: expectedTitle,
+          isCompleted: expectedIsCompleted,
+        );
 
-      //  assert
-      final verification =
-          verify(() => todoApiMock.saveTodo(captureAny(), captureAny()))
-              .captured;
+        //  assert
+        final verification = verify(
+          () => todoApiMock.saveTodo(captureAny(), captureAny()),
+        );
+        expect(verification.callCount, 1);
 
-      expect(verification.first, expectedTitle);
-      expect(verification.last, expectedIsCompleted);
-    });
+        final capturedArguments = verification.captured;
+        expect(capturedArguments.first, expectedTitle);
+        expect(capturedArguments.last, expectedIsCompleted);
+      },
+    );
   });
 }
 
